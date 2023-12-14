a0c148788a98a4c0673d5bb02136c6cf5f5b12b7
yarik <yarik@HOME-PC>
2023-12-14 10:59:06 +0200

    Добавьте файлы проекта.

---
+++ Task2/Task2.cpp
@@ -0,0 +1,21 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+int countCharacters(const std::string& str) {
+    return str.size();
+}
+
+int main() {
+    std::string inputString;
+
+    std::getline(std::cin, inputString);
+
+    int characterCount = countCharacters(inputString);
+
+    std::cout << "Кількість символів у рядку: " << characterCount << std::endl;
+
+    return 0;
+}
